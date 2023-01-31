e6406730a85d248071a4d641a7d2682234151acc
Akshay Bagai <100753870+Akshaybagai52@users.noreply.github.com>
2023-01-31 14:08:33 +0530

    completed with all functionality

--- index.js
+++ index.js
@@ -10,25 +10,25 @@ const replaceVal = (tempVal, orgVal) => {
     temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max)
     temperature = temperature.replace("{%location%}", orgVal.name)
     temperature = temperature.replace("{%country%}", orgVal.sys.country)
+    return temperature
 
 
 }
 
 const server = http.createServer((req, res) => {
-    if (req.url == "/") {
+    if (req.url === "/") {
         requests('https://api.openweathermap.org/data/2.5/weather?q=Pune&units=metric&appid=5366ece91f3b8800583543c4aa4f17c1')
             .on('data', (chunk) => {
                 const objdata = JSON.parse(chunk)
                 const arrdata = [objdata]
 
-                const realTimeData = arrdata.map((val) => {
-                    replaceVal(homeFile, val)
-                })
+                const realTimeData = arrdata.map((val) => replaceVal(homeFile, val)).join("")
+                res.write(realTimeData)
+                // console.log(realTimeData)
             })
             .on('end', (err) => {
                 if (err) return console.log('connection closed due to errors', err);
-
-                console.log('end');
+                res.end()
             });
     }
 
